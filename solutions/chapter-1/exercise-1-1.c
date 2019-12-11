83f668e857f13bcfe6d50d46f148eb8229a0db8b
Konstantin Kotsarev <kotsarev@gmail.com>
2019-12-11 20:52:20 +0300

    Exercise 1-1

---
+++ solutions/chapter-1/exercise-1-1.c
@@ -0,0 +1,15 @@
+/*
+K&R2, chapter 1, section 1.1 Getting Started, exercise 1-1
+
+Run the ``hello, world'' program on your system.
+Experiment with leaving out parts of the program, to see what error messages you get.
+
+Created by Konstantin Kotsarev on 2019-12-11
+*/
+
+#include <stdio.h>
+
+main()
+{
+  printf("hello, world\n");
+}
