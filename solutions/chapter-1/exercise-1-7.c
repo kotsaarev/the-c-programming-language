8f4e716424ee3dc603032c0008a8737363a079b8
Konstantin Kotsarev <kotsaarev@gmail.com>
2022-03-19 16:24:33 +0700

    Exercise 1-7

---
+++ solutions/chapter-1/exercise-1-7.c
@@ -0,0 +1,14 @@
+/*
+K&R2, chapter 1, section 1.5 Character Input and Output, exercise 1-7
+
+Write a program to print the value of EOF.
+
+Created by Konstantin Kotsarev on 2022-03-19
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("The value of EOF is %d\n", EOF);
+}
